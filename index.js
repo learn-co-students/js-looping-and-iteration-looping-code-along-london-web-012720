51d637f4441e70cfcb518bd3449350bcac74f1b4
Sergio Robles <sroblesc@gmail.com>
2020-03-09 16:37:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+const result = [];
+
+function writeCards(names,birthday) {
+    for (let i = 0; i < names.length; i++) {
+        result.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`);
+                    
+    };
+    return result
+};
+function countDown(number) {
+    let i = -1
+    while (i<number) {
+        console.log(number--)
+    }
+}
